8c22014c9083299e9502c104dacde53437b105a6
Mauricio Pereira <mauricio.eberlin@hotmail.com>
2024-01-30 10:53:34 -0300

    #01 - Projeto Spring Boot gerado com o Spring Initializr

---
+++ agendamento-transferencias-backend/src/main/java/com/mauricio/projeto/AgendamentoTransferenciasApplication.java
@@ -0,0 +1,13 @@
+package com.mauricio.projeto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendamentoTransferenciasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendamentoTransferenciasApplication.class, args);
+	}
+
+}
---
+++ agendamento-transferencias-backend/src/test/java/com/mauricio/projeto/AgendamentoTransferenciasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mauricio.projeto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendamentoTransferenciasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
